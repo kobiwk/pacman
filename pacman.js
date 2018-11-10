f855a9c0f9b5178aede324b64e2ca256c3247fdf
Rastislav Kobac <rastislav.kobac@gmail.com>
2018-11-10 15:28:28 +0100

    X and Y variables changed names, and basic rules nested in one object"
    "

--- pacman.js
+++ pacman.js
@@ -18,129 +18,36 @@
 
 	let context = pacmanWorld.getContext('2d');
 
-	/* Create board, game title and score */
-	var createBoard = function() {
-		context.fillStyle = "blue";
-		context.fillRect( 0, 0, pacmanWidth, pacmanHeight );
-		context.fillStyle = "white";
-		context.font = "20px Verdana";
-		context.fillText("Pacman: " + pacmanLives + " Score: " + score, 10, 30);
-		context.fillStyle = "blue";
-	}
-
-	function resultOfGame(obj) {
-		var that = obj;
-		//touch of the enemies...
-		if ( x <= (that.x + 26) && that.x <= ( x + 26 ) && y <= ( that.y + 26 ) && that.y <= ( y + 26) ) {
-			//... takes Pacman lives
-			pacmanLives--;
-			//clear background and add new text. Primitive.
+	let BasicRules = {
+		/* Create board, game title and score */
+		createBoard: function() {
 			context.fillStyle = "blue";
-			context.fillRect( 0, 0, pacmanWidth, 40 );
+			context.fillRect( 0, 0, pacmanWidth, pacmanHeight );
 			context.fillStyle = "white";
 			context.font = "20px Verdana";
 			context.fillText("Pacman: " + pacmanLives + " Score: " + score, 10, 30);
 			context.fillStyle = "blue";
-			detectCollison(new DefinePacman, obj);
-			
-			//send Pacman to default position
-			context.fillRect( x, y, 32, 32 );
-			x = 0;
-			y = 40;
-			// stop moving of Pacman, after he collide with an enemy
-			if ( keyState[37] || keyState[38] || keyState[39] || keyState[40] ) {
-				keyState[37] = false;
-				keyState[38] = false;
-				keyState[39] = false;
-				keyState[40] = false;
-			}
-
-			//THE END
-			if ( pacmanLives == 0 ) {
-				//block moving of enemies
-				blockEnemy = true;
-			}
-		}
-	}
-
-	// detect when enemy kills poor Pacman
-	function detectCollison(pacman, enemy) {
-		if ( x <= (enemy.x + 26) && enemy.x <= ( x + 26 ) && y <= ( enemy.y + 26 ) && enemy.y <= ( y + 26 ) ) {
-			alert("Houston, we got a problem!");
-		}
-
-	}
-
-	// detect when enemy is hit with Pacman's weapon
-	function detectHit( move_direction, attackX, attackY, weapon, obj ) {
-	
-		if ( direction === move_direction ) {
-			let attack = setInterval( function() {
-
-			if ( move_direction === "right" ) {
-				context.fillRect(attackX+32, attackY, 32, 32);
-				attackX += 5;
-
-				context.drawImage(weapon, 0, 0, 520, 520, attackX+32, attackY, 32, 32);
+		},
+		resultOfGame: function(obj) {
+			var that = obj;
+			//touch of the enemies...
+			if ( PacmanX <= (that.x + 26) && that.x <= ( PacmanX + 26 ) && PacmanY <= ( that.y + 26 ) && that.y <= ( PacmanY + 26) ) {
+				//... takes Pacman lives
+				pacmanLives--;
+				//clear background and add new text. Primitive.
+				context.fillStyle = "blue";
+				context.fillRect( 0, 0, pacmanWidth, 40 );
+				context.fillStyle = "white";
+				context.font = "20px Verdana";
+				context.fillText("Pacman: " + pacmanLives + " Score: " + score, 10, 30);
+				context.fillStyle = "blue";
+				BasicRules.detectCollison(new DefinePacman, obj);
 				
-				//clear weapon when it leave the board
-				if ( attackX >= 700 ) clearInterval( attack );			
-			}
-
-			if ( move_direction === "left" ) {
-				context.fillRect(attackX-32, attackY, 32, 32);
-						attackX -= 5;
-
-				context.drawImage(weapon, 0, 0, 520, 520, attackX-32, attackY, 32, 32);
-				//clear weapon when it leave the board
-				if ( attackX <= 0 ) clearInterval( attack );	
-			}
-
-			if ( move_direction === "up" ) {
-				context.fillRect(attackX, attackY-32, 32, 32);
-				attackY -= 5;
-
-				context.drawImage(weapon, 0, 0, 520, 520, attackX, attackY-32, 32, 32);
-
-				//clear weapon when it leave the board
-				if ( attackY <= 60 ) {
-					clearInterval( attack );
-					context.fillRect(attackX, 30, 32, 32);
-				}	
-			}
-			
-			if ( move_direction === "down" ) {
-				let attack = setInterval( function() {
-
-					context.fillRect(attackX, attackY+32, 32, 32);
-					attackY += 5;
-
-					context.drawImage(weapon, 0, 0, 520, 520, attackX, attackY+32, 32, 32);
-					
-					//clear weapon when it leave the board
-					if ( attackY >= 512 ) {
-						clearInterval( attack );
-						//context.fillRect(attackX, 60, 32, 32);
-					}
-				}, 10);
-			}
-
-			//detect when weapon hit the target
-			if ( attackX <= (obj.x + 26) && obj.x <= ( attackX + 26 ) && attackY <= ( obj.y + 26 ) && obj.y <= ( attackY + 26 ) ) {
-				//alert("Die motherfucker!");
-				test = true;
-				/* create new Enemy after current Enemy is hit */
-				EnemyGhost = new CreateEnemy(400, 400, 10, 128, 0, 3);
-				EnemyGhost.load();
-
-				//remove enemy after hit, and bring him back after 5 seconds, on other position
-	
-				context.fillRect( obj.x, obj.y, 32, 32 );
-				
-				obj.x = 1600;
-				obj.y = 40;
-				
-				// stop moving of Pacman, after he crush his enemy
+				//send Pacman to default position
+				context.fillRect( x, y, 32, 32 );
+				PacmanX = 0;
+				PacmanY = 40;
+				// stop moving of Pacman, after he collide with an enemy
 				if ( keyState[37] || keyState[38] || keyState[39] || keyState[40] ) {
 					keyState[37] = false;
 					keyState[38] = false;
@@ -148,22 +55,110 @@
 					keyState[40] = false;
 				}
 
+				//THE END
+				if ( pacmanLives == 0 ) {
+					//block moving of enemies
+					blockEnemy = true;
+				}
 			}
-			}, 10);
-		}
-	} 
+		},
+		/* detect when enemy kills poor Pacman */
+		detectCollison: function(pacman, enemy) {
+			if ( PacmanX <= (enemy.x + 26) && enemy.x <= ( PacmanX + 26 ) && PacmanY <= ( enemy.y + 26 ) && enemy.y <= ( PacmanY + 26 ) ) {
+				alert("Houston, we got a problem!");
+			}
+		},
+		/* detect when enemy is hit with Pacman's weapon */
+		detectHit: function( move_direction, attackX, attackY, weapon, obj ) {
+	
+			if ( direction === move_direction ) {
+				let attack = setInterval( function() {
 
-	let x = 0;
-	let y = 40;
+				if ( move_direction === "right" ) {
+					context.fillRect(attackX+32, attackY, 32, 32);
+					attackX += 5;
+
+					context.drawImage(weapon, 0, 0, 520, 520, attackX+32, attackY, 32, 32);
+					
+					//clear weapon when it leave the board
+					if ( attackX >= 700 ) clearInterval( attack );			
+				}
+
+				if ( move_direction === "left" ) {
+					context.fillRect(attackX-32, attackY, 32, 32);
+							attackX -= 5;
+
+					context.drawImage(weapon, 0, 0, 520, 520, attackX-32, attackY, 32, 32);
+					//clear weapon when it leave the board
+					if ( attackX <= 0 ) clearInterval( attack );	
+				}
+
+				if ( move_direction === "up" ) {
+					context.fillRect(attackX, attackY-32, 32, 32);
+					attackY -= 5;
+
+					context.drawImage(weapon, 0, 0, 520, 520, attackX, attackY-32, 32, 32);
+
+					//clear weapon when it leave the board
+					if ( attackY <= 60 ) {
+						clearInterval( attack );
+						context.fillRect(attackX, 30, 32, 32);
+					}	
+				}
+				
+				if ( move_direction === "down" ) {
+					let attack = setInterval( function() {
+
+						context.fillRect(attackX, attackY+32, 32, 32);
+						attackY += 5;
+
+						context.drawImage(weapon, 0, 0, 520, 520, attackX, attackY+32, 32, 32);
+						
+						//clear weapon when it leave the board
+						if ( attackY >= 512 ) {
+							clearInterval( attack );
+							//context.fillRect(attackX, 60, 32, 32);
+						}
+					}, 10);
+				}
+
+				//detect when weapon hit the target
+				if ( attackX <= (obj.x + 26) && obj.x <= ( attackX + 26 ) && attackY <= ( obj.y + 26 ) && obj.y <= ( attackY + 26 ) ) {
+					//alert("Die motherfucker!");
+					test = true;
+					/* create new Enemy after current Enemy is hit */
+					EnemyGhost = new CreateEnemy(400, 400, 10, 128, 0, 3);
+					EnemyGhost.load();
+
+					//remove enemy after hit, and bring him back after 5 seconds, on other position
+		
+					context.fillRect( obj.x, obj.y, 32, 32 );
+					
+					obj.x = 1600;
+					obj.y = 40;
+					
+					// stop moving of Pacman, after he crush his enemy
+					if ( keyState[37] || keyState[38] || keyState[39] || keyState[40] ) {
+						keyState[37] = false;
+						keyState[38] = false;
+						keyState[39] = false;
+						keyState[40] = false;
+					}
+
+				}
+				}, 10);
+			}
+		} 
+	}
+
+	let PacmanX = 0;
+	let PacmanY = 40;
 	let PacmanSpeed = 10;
 	let direction = 0;
 	let keyState = {};
 
 	/* variables to define position of pacman */
 	function DefinePacman() {
-		this.x = 0;
-		this.y = 40;
-		this.PacmanSpeed = 10;
 	}
 
 	DefinePacman.prototype = {
@@ -172,7 +167,7 @@
 			loadBeignsImages.src = "pac.png";
 			return loadBeignsImages.onload = function() {
 				//default
-				if ( x % 2 === 0 ) {
+				if ( PacmanX % 2 === 0 ) {
 					context.drawImage(loadBeignsImages, 320, 0, 32, 32, 0, 40, 32, 32);
 				} 
 			}
@@ -181,18 +176,18 @@
 			var that = this;
 			document.addEventListener('keypress', function(event) {
 				//let currentPosition = x;
-				let attackX = x;
-				let attackY = y;
+				let attackX = PacmanX;
+				let attackY = PacmanY;
 				let weap = new Image();
 				let attackSpeed = 100;
 				weap.src = "ask-hand.png";
 				context.fillStyle = "blue";
 				 
 				if ( event.keyCode === 32 ) {
-					detectHit( "right", attackX, attackY, weap, obj );
-					detectHit( "left", attackX, attackY, weap, obj );
-					detectHit( "up", attackX, attackY, weap, obj );
-					detectHit( "down", attackX, attackY, weap, obj );
+					BasicRules.detectHit( "right", attackX, attackY, weap, obj );
+					BasicRules.detectHit( "left", attackX, attackY, weap, obj );
+					BasicRules.detectHit( "up", attackX, attackY, weap, obj );
+					BasicRules.detectHit( "down", attackX, attackY, weap, obj );
 				};
 			}, false);
 		},
@@ -209,16 +204,16 @@
 			clearPos.stayInside();
 			if ( pacmanLives > 0) {
 				if ( keyState[39] ) {
-					x+=PacmanSpeed;
+					PacmanX+=PacmanSpeed;
 					direction = "right";
 				} else if ( keyState[40] ) {				
-					y+=PacmanSpeed;
+					PacmanY+=PacmanSpeed;
 					direction = "down";	
 				} else if ( keyState[37] ) {					
-					x-=PacmanSpeed;
+					PacmanX-=PacmanSpeed;
 					direction = "left";
 				} else if ( keyState[38] ) {					
-					y-=PacmanSpeed;
+					PacmanY-=PacmanSpeed;
 					direction = "up";
 				}
 			} else {
@@ -234,45 +229,45 @@
 			//draw Pacman one step ahead
 			if ( openMouth % 2 ===  0 ) {
 				if ( direction === "right" ) {
-					context.drawImage(loadBeignsImages, 320, 0, 32, 32, x, y, 32, 32);
+					context.drawImage(loadBeignsImages, 320, 0, 32, 32, PacmanX, PacmanY, 32, 32);
 				} else if ( direction === "left" ) {
-					context.drawImage(loadBeignsImages, 352, 64, 32, 32, x, y, 32, 32);
+					context.drawImage(loadBeignsImages, 352, 64, 32, 32, PacmanX, PacmanY, 32, 32);
 				} else if ( direction === "down" ) {
-					context.drawImage(loadBeignsImages, 320, 32, 32, 32, x, y, 32, 32);
+					context.drawImage(loadBeignsImages, 320, 32, 32, 32, PacmanX, PacmanY, 32, 32);
 				} else if ( direction === "up" ) {
-					context.drawImage(loadBeignsImages, 320, 96, 32, 32, x, y, 32, 32);
+					context.drawImage(loadBeignsImages, 320, 96, 32, 32, PacmanX, PacmanY, 32, 32);
 				}
 				
 			} else if ( openMouth % 2 ===  1 ) {
 				if ( direction === "right" ) {
-				context.drawImage(loadBeignsImages, 352, 0, 32, 32, x, y, 32, 32);
+				context.drawImage(loadBeignsImages, 352, 0, 32, 32, PacmanX, PacmanY, 32, 32);
 				} else if ( direction === "left" ) {
-					context.drawImage(loadBeignsImages, 320, 64, 32, 32, x, y, 32, 32);
+					context.drawImage(loadBeignsImages, 320, 64, 32, 32, PacmanX, PacmanY, 32, 32);
 				} else if ( direction === "down" ) {
-					context.drawImage(loadBeignsImages, 352, 32, 32, 32, x, y, 32, 32);
+					context.drawImage(loadBeignsImages, 352, 32, 32, 32, PacmanX, PacmanY, 32, 32);
 				} else if ( direction === "up" ) {
-					context.drawImage(loadBeignsImages, 352, 96, 32, 32, x, y, 32, 32);
+					context.drawImage(loadBeignsImages, 352, 96, 32, 32, PacmanX, PacmanY, 32, 32);
 				}
 			}
 			setTimeout( clearPos.move,50 );
 		},
 		clearPacmanPreviousPosition: function(){
-			let currentpositionX = x;
-			let currentpositionY = y;
+			let currentpositionX = PacmanX;
+			let currentpositionY = PacmanY;
 			return context.fillRect(currentpositionX, currentpositionY, 32, 32);
 		},
 		stayInside: function(obj) {
-			if ( x > pacmanWidth) {
-				x = 0;
+			if ( PacmanX > pacmanWidth) {
+				PacmanX = 0;
 			}
-			if ( x < 0 ) {
-				x = pacmanWidth;
+			if ( PacmanX < 0 ) {
+				PacmanX = pacmanWidth;
 			}
-			if ( y > pacmanHeight ) {
-				y = 40;
+			if ( PacmanY > pacmanHeight ) {
+				PacmanY = 40;
 			}
-			if ( y < 40 ) {
-				y = pacmanHeight;
+			if ( PacmanY < 40 ) {
+				PacmanY = pacmanHeight;
 			}
 		}
 	}
@@ -326,7 +321,7 @@ document.addEventListener('keyup', function(event){
 			function iLikeToMoveIt() {
 				//start default movement
 				if ( blockEnemy === false ) {
-					resultOfGame(that);
+					BasicRules.resultOfGame(that);
 					controlNMEMoving++;
 					if ( controlNMEMoving == 20 ) {
 						direction = that.choseDirection();
@@ -439,7 +434,7 @@ document.addEventListener('keyup', function(event){
 
 
 	function init() {
-		createBoard();
+		BasicRules.createBoard();
 		//new Object
 		
 		//create Beigns
